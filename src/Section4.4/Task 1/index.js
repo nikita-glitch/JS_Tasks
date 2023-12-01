8d89f5741f9bd9eb2723e17ced8421db860f2a10
fusion-team <nschicherin@gmail.com>
2023-12-01 17:27:01 +0300

    5.1

--- src/Section4.4/Task 1/index.js
+++ src/Section4.4/Task 1/index.js
@@ -1,5 +1,5 @@
 let formContainer = document.getElementById("prompt-form-container");
-let form = document.getElementById("promt-form");
+let form = document.getElementById("prompt-form");
 let formText = document.getElementById("prompt-message");
 formContainer.hidden = true;
 let button = document.createElement("button");
@@ -8,18 +8,14 @@ document.body.append(button);
 
 button.addEventListener("click", (e) => {
   formContainer.hidden = false;
- 
+
   showPrompt("Введите что-нибудь<br>...умное :)", function (value) {
     alert(value);
   });
-
-
 });
 function showPrompt(line, callback) {
   formText.innerHTML = line;
   form.addEventListener("submit", (e) => {
-    
-
     let [input] = document.getElementsByName("text");
 
     let value = input.value;
@@ -28,6 +24,20 @@ function showPrompt(line, callback) {
     }
 
     callback(value);
-    return false;
   });
+}
+let length = form.elements.length - 1;
+let first = form.elements[0]
+let last = form.elements[length]
+first.onkeydown = (e) => {
+  if ( e.key == 'Tab' && !e.shiftKey) {
+    first.focus();
+    return false;
+  } 
+}
+last.onkeydown = (e) => {
+  if((e.key == 'Tab' && e.shiftKey)) {
+    last.focus();
+    return false;
+  }
 }
